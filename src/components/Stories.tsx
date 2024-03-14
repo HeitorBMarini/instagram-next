10e9dfb6053695b5e8ad4c46e41ff12f4d34936e
HeitorBMVanguard <heitorborba@sistemavanguard.com.br>
2024-03-14 20:48:37 -0300

    Esrtulizando scrollbar dos stories e fotos

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -15,13 +15,14 @@ export default function Stories() {
   useEffect(() => {
     const storyUsers = minifaker.array(20, (i: any) => ({
       username: minifaker.username({ locale: "en" }).toLowerCase(),
-      img: `https://i.pravatar.cc/150?img=3${Math.ceil(Math.random() * 10)}`,
+      img: `https://i.pravatar.cc/150?img=3${Math.ceil(Math.random() * 9)}`,
       id: i,
     }));
     setSoryUsers(storyUsers);
     console.log(storyUsers)
   }, []);
-  return <div>
+  return <div className="flex space-x-2 p-6 bg-white
+  border-gray-200 mt-8 border overflow-x-scroll rounded-sm no-scrollbar" >
     {storyUsers.map(user=> (
       <Story key={user.id} username={user.username} img={user.img}
       />
